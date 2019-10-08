44f72dd383019f472c5495a21638d463a16e7c76
danielvcorreia <danielvalacorreia@ua.pt>
2019-10-08 19:07:17 +0100

    Needs testing

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
@@ -14,44 +14,40 @@ namespace sofs19
        the first 2 entries are filled in with "." and ".." references
        the other entries are empty.
        If rdsize is 2, a second block exists and should be filled as well.
-       */
+    */
     void grpFillRootDir(uint32_t itotal)
     {
         soProbe(606, "%s(%u)\n", __FUNCTION__, itotal);
 
         /* change the following line by your code */
-        /* return binFillRootDir(itotal); */
 
         /* DPB number of Dir Struct References to fill a Data Block */
         /* SODirEntry buffer */
         SODirEntry entryBlockDirs[DPB];
 
-        /* Filling all the SODirEntry's with zeros in Data Field "in" 
-           and NullReference in Data Field "namespace" */
-
-        /* DÚVIDA SE É "NULL" OU "NullReference" */
-        /* DÚVIDA SE SERIA PREFERIVÉL UTILIZAR "memset()" e "memcpy" */
-
+        /* Filling all the SODirEntry's(Block) with zeros */
+        /* Memset() is more eficient than manual changing all data fields "in" to zeros */
+        memset( entryBlockDirs, 0, BlockSize );
+        
+        /* Filling with "NullReference" Data Field "namespace" in all SODirEntry dir's */
         for (uint32_t i = 2; i < DPB; i++)
-        { entryBlockDirs[i] -> in = 0; entryBlockDirs[i] -> namespace = NULL; }
+        { entryBlockDirs[i] -> in = NullReference; }
 
         /* Setting the first SODirEntry to "." and the second one to ".." */
+        entryBlockDirs[0] -> name = ".";
+        entryBlockDirs[1] -> name = "..";
 
-        /* DÚVIDA DE VALOR PARA CAMPO "IN" */
-        entryBlockDirs[0] -> in = 0;
-        entryBlockDirs[0] -> namespace = ".";
-        entryBlockDirs[1] -> in = 0;
-        entryBlockDirs[1] -> namespace = "..";
-
-        /* Writing in the block position itotal + 1 (SuperBlock) in the raw disk */
-        soWriteRawBlock( itotal+1, entryBlockDirs );
+        /* Writing the block "inodesTotal/InodePerBlock + 1(SuperBlock)" in the raw disk */
+        /* The division between itotal and IPB is always with rest 0 */
+        soWriteRawBlock( itotal/IPB + 1, entryBlockDirs );
         
         if (rdsize == 2)
         {
             SODirEntry entryBlock2Dirs[DPB];
+            memset( entryBlock2Dirs, 0, BlockSize );
             for(uint32_t i = 0; i < DPB; i++)
-            { entryBlock2Dirs[i] -> in = 0; entryBlock2Dirs[i] -> namespace = NULL; }
-            soWriteRawBlock( itotal+2, entryBlock2Dirs );
+            { entryBlock2Dirs[i] -> in = NullReference; }
+            soWriteRawBlock( itotal/IPB + 2, entryBlock2Dirs );
         }
     }
 };
