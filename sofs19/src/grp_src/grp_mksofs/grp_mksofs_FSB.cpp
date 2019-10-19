04cd6bc3a51d00b9a2411a5b84154b806149731c
José Silva <jose.silva99@ua.pt>
2019-10-19 16:53:10 +0100

    [mksofs] FSB quase feito não testado

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FSB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FSB.cpp
@@ -14,6 +14,40 @@ namespace sofs19
         soProbe(602, "%s(%s, %u, %u, %u)\n", __FUNCTION__, name, ntotal, itotal, nbref);
 
         /* change the following line by your code */
+        SOSuperBlock sb;
+        sb.magic = MAGIC_NUMBER;
+        sb.version = VERSION_NUMBER;
+        sb.name = *name;
+        sb.mntstat = 1; // maybe
+        sb.mntcnt = 0;
+        sb.ntotal = ntotal;
+
+        // Inodes metadata
+        sb.it_size = itotal/IPB;
+        sb.itotal = itotal;
+        sb.ifree = itotal - 1;
+        sb.ihead = 1; // maybe
+        sb.itail = itotal - 1; // maybe
+
+        // Data blocks metadata
+        sb.dz_start = sb.it_size + 1;
+        sb.dz_total = sb.ntotal - sb.it_size - 1;
+        sb.free = sb.ntotal - nbref;
+        sb.head_blk = 1;
+        sb.head_idx = 1;                                   
+        sb.tail_blk = nbref;
+        // sb.tail_idx = 256 - something 
+        
+        HeadCache head;
+        for(int i = 0; i < HEAD_CACHE_SIZE; i++)
+            head.ref[i] = nbref + 1 + i;
+        head.idx = nbref + 1; // maybe
+
+        TailCache tail;
+        for(int i = 0; i < TAIL_CACHE_SIZE; i++)
+            tail.ref[i] = NullReference;
+        tail.idx = 0; // maybe
+
         binFillSuperBlock(name, ntotal, itotal, nbref);
     }
 };
