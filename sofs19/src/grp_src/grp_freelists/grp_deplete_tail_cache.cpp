0d208d7594759584f348f664b08cfa5c2bef87a9
o2valente <pedro.valente@ua.pt>
2019-10-21 01:32:17 +0100

    finished CS

--- sofs19/src/grp_src/grp_freelists/grp_deplete_tail_cache.cpp
+++ sofs19/src/grp_src/grp_freelists/grp_deplete_tail_cache.cpp
@@ -16,6 +16,22 @@
 #include <iostream>
 using namespace std;
 
+/**
+ *  Replenish the head cache.
+
+    References to free data blocks should be transferred from the head reference data block or from the tail cache, if no reference data blocks exist, to the head cache:
+
+    Remarks
+        nothing is if the cache is not empty;
+        the tail cache is only used if there are no reference data blocks;
+        only a single block is processed, even if it is not enough to fulfill the head cache;
+        the block processed is the one pointed to by the head_blk field of the superblock;
+        after transferring a reference from A to B, the value in A becomes NullReference;
+        if after the replenish the head reference data block gets empty, the head passes to the next, if one exists, and the previous is freed;
+        in the previous situation, all entries of the freed data block sre set to NullReference;
+        when calling a function of any layer, the version with prefix so is used.
+ */
+
 namespace sofs19
 {
     /* only fill the current block to its end */
--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_CS.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_CS.cpp
@@ -8,8 +8,8 @@
 
 namespace sofs19
 {
-    void grpComputeStructure(uint32_t ntotal, uint32_t & itotal, uint32_t & nbref)
-
+    void grpComputeStructure(uint32_t ntotal, uint32_t & itotal, uint32_t & nbref){
+        soProbe(601, "%s(%u, %u, ...)\n", __FUNCTION__, ntotal, itotal);
     /*
       Remarks
         - this function does not change any block of the disk;
@@ -22,32 +22,24 @@ namespace sofs19
             single data block remains, it is assigned to the inode table;
      */
 
-    {
-       soProbe(601, "%s(%u, %u, ...)\n", __FUNCTION__, ntotal, itotal);
-            
              /* change the following line by your code */
-            binComputeStructure(ntotal, itotal, nbref);
+            //binComputeStructure(ntotal, itotal, nbref);
 
-            // if it is zero initially, the value ntotal/16 should be used as the start value for itotal, 
-            // where / stands for the integer division
-            if (itotal == 0) itotal = ntotal/16;
-            // itotal is always lower than or equal to ntotal/8
-            else if (itotal > ntotal/8) itotal = ntotal/8;
-            // itotal is always greater than or equal to IPB
-            else if (itotal < IPB) itotal = IPB;
-            // itotal must be rounded up to be multiple of IPB
-            while(itotal % IPB != 0) itotal = itotal + 1; 
-            
-            // 1 representa o superblock
-            // itotal/IPB representa o numero de blocos dos inodes
-            // segundo 1 representa a raiz
-            // 64 representa a head cache size
-            ntotal = (ntotal - 1 - (itotal/IPB) - 1 - 64);
+            if(itotal == 0 || itotal > ntotal/8 || itotal < IPB) itotal = ntotal/16;
 
-            // se o resultado da divisão do ntotal pelo RPB for diferente de 1 os nossos cálculos 
-            // estão feitos, senão, teremos que incrementar o numero de blocos de inodes e refazer os cálculos
-            if(ntotal % RPB != 1) ntotal = ntotal / RPB;
-            while(ntotal % RPB == 1) ntotal = ((ntotal - 1 - ((itotal/IPB) + 1 ) - 1 - 64)/RPB);
+            if(itotal % IPB == 0) itotal = itotal/IPB;
+            else{
+                while(itotal % IPB != 0) itotal = itotal + 1;
+            }
+
+            uint32_t ib = itotal/IPB;
+
+            if((1 + ib + 1 + HEAD_CACHE_SIZE) >= ntotal) nbref = 0;
+            else{
+                if((ntotal - 1 - ib - 1 - HEAD_CACHE_SIZE) % RPB == 1) nbref = ((ntotal - 1 - ib - 1 - HEAD_CACHE_SIZE) - 1) / RPB;
+                else nbref = (ntotal - 1 - ib - 1 - HEAD_CACHE_SIZE) / RPB+1;
+            }
+
     }
 };
 
