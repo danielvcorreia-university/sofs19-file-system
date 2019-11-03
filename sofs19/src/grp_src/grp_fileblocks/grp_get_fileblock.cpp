e680080f285a917a14126d32a8e3e63fc261e163
Joaquim Ramos <ramosquimjr@gmail.com>
2019-11-03 18:03:41 +0000

    GetFileBlock nao testado
    ainda nao esta testado por isso esta comentado

--- sofs19/src/grp_src/grp_fileblocks/grp_get_fileblock.cpp
+++ sofs19/src/grp_src/grp_fileblocks/grp_get_fileblock.cpp
@@ -21,6 +21,30 @@ namespace sofs19
     {
         soProbe(301, "%s(%d, %u)\n", __FUNCTION__, ih, fbn);
 
+        /*
+        SOInode* ip = soGetInodeNumber(ih);
+			uint32_t IndirectBegin = N_DIRECT;
+			uint32_t DoubleIndirectBegin = (N_INDIRECT * RPB) + IndirectBegin;
+			uint32_t DoubleIndirectEnd = (RPB * RPB * N_DOUBLE_INDIRECT) + DoubleIndirectBegin -1;
+
+			if(fbn >= 0 && fbn <= DoubleIndirectEnd){
+
+				if(fbn < IndirectBegin){
+					return ip->d[fbn];
+				}
+				else if(fbn < DoubleIndirectBegin){
+					return grpGetIndirectFileBlock(ip,fbn-IndirectBegin);
+				}
+				else {
+					return grpGetDoubleIndirectFileBlock(ip,fbn-DoubleIndirectBegin);
+				}
+
+			}
+			else{
+				throw SOException(EINVAL, __FUNCTION__);
+			}
+            */
+
         /* change the following line by your code */
         return binGetFileBlock(ih, fbn);
     }
@@ -32,9 +56,17 @@ namespace sofs19
     {
         soProbe(301, "%s(%d, ...)\n", __FUNCTION__, afbn);
 
-        /* change the following two lines by your code */
-        throw SOException(ENOSYS, __FUNCTION__); 
-        return 0;
+            uint32_t db[RPB];
+            uint32_t pos1=afbn / RPB;
+            uint32_t pos2= afbn % RPB;
+
+            if(ip->i1[pos1] == NullReference){
+            	return NullReference;
+            }
+            else{
+                soReadDataBlock(ip->i1[pos1],&db);
+                return db[pos2];
+            }
     }
 #endif
 
@@ -45,9 +77,26 @@ namespace sofs19
     {
         soProbe(301, "%s(%d, ...)\n", __FUNCTION__, afbn);
 
-        /* change the following two lines by your code */
-        throw SOException(ENOSYS, __FUNCTION__); 
-        return 0;
+            uint32_t db[RPB];
+            uint32_t pos1 = afbn / (RPB*RPB);
+            uint32_t pos2 = afbn / RPB-(pos1*RPB);
+            uint32_t pos3 = afbn  % RPB;
+
+            if(ip->i2[pos1] == NullReference){
+            	return NullReference;
+            }
+            else{
+                soReadDataBlock(ip->i2[pos1],&db);
+
+                if(db[pos2] == NullReference){
+                	return NullReference;
+                }
+                else{
+                	soReadDataBlock(db[pos2],&db);
+                	return db[pos3];
+                }
+
+            }
     }
 #endif
 };
