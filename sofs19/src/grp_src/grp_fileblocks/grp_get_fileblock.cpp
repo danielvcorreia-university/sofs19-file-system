c3e8ba8f0beec59c784a97968e0a5bec67e404e5
Joaquim Ramos <ramosquimjr@gmail.com>
2019-11-11 11:54:57 +0000

    Correção na chamada da funçao da camada dal, na funçao get fIleblock

--- sofs19/src/grp_src/grp_fileblocks/grp_get_fileblock.cpp
+++ sofs19/src/grp_src/grp_fileblocks/grp_get_fileblock.cpp
@@ -22,7 +22,7 @@ namespace sofs19
         soProbe(301, "%s(%d, %u)\n", __FUNCTION__, ih, fbn);
 
         /*
-        SOInode* ip = soGetInodeNumber(ih);
+            SOInode* ip = soGetInodePointer(ih);
 			uint32_t IndirectBegin = N_DIRECT;
 			uint32_t DoubleIndirectBegin = (N_INDIRECT * RPB) + IndirectBegin;
 			uint32_t DoubleIndirectEnd = (RPB * RPB * N_DOUBLE_INDIRECT) + DoubleIndirectBegin -1;
