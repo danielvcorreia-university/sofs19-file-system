cd55cf03dadc2c5bd142ef5cec5c7fa131162c2f
Daniel <daniel-correia@live.com.pt>
2019-11-11 12:33:16 +0000

    DeleteDirEntry semi-testado

--- sofs19/src/grp_src/grp_direntries/grp_delete_direntry.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_delete_direntry.cpp
@@ -13,10 +13,30 @@ namespace sofs19
 {
     uint32_t grpDeleteDirEntry(int pih, const char *name)
     {
-        soProbe(203, "%s(%d, %s)\n", __FUNCTION__, pih, name);
 
-        /* change the following line by your code */
-        return binDeleteDirEntry(pih, name);
+        SODirEntry dirEntrys[DPB];
+
+        soProbe(203, "%s(%d, %s)\n", __FUNCTION__, pih, name);
+        SOInode *inode = soGetInodePointer(pih);
+
+        uint32_t ret;
+
+        for(uint32_t i = 0; i < inode->blkcnt; i++){
+            soReadFileBlock(pih, i, dirEntrys);
+            for(uint32_t j = 0; j < DPB; j++){
+                if(strcmp(dirEntrys[j].name, name) == 0){
+                    for(uint32_t k = 0; k < SOFS19_MAX_NAME; k++) {
+                        dirEntrys[j].name[k] = '\0';
+                    }
+                    ret = dirEntrys[j].in;
+                    dirEntrys[j].in = NullReference;
+                    soWriteFileBlock(pih, i, dirEntrys);
+                    return ret;
+                }
+            }
+        }
+
+        throw SOException(ENOENT, __FUNCTION__);
     }
 };
 
