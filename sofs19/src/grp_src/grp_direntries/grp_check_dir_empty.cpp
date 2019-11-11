3358b35d1055bf2d1f85e384e9d0bb0f7f180e63
o2valente <pedro.valente@ua.pt>
2019-11-11 12:42:40 +0000

    write_fileblock & check_direntry - not tested

--- sofs19/src/grp_src/grp_direntries/grp_check_dir_empty.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_check_dir_empty.cpp
@@ -16,7 +16,38 @@ namespace sofs19
         soProbe(205, "%s(%d)\n", __FUNCTION__, ih);
 
         /* change the following line by your code */
-        return binCheckDirEmpty(ih);
+        //return binCheckDirEmpty(ih);
+
+        SOInode* node = soGetInodePointer(ih);
+
+        uint32_t block = (node->size) / BlockSize;
+
+        SODirEntry buf[DPB];
+
+        uint32_t blockCount = 0;
+        uint32_t blockNum = 0;
+        uint32_t tmp;
+
+        while(blockCount < block){
+            tmp = soGetFileBlock(ih,blockNum);
+
+            if(tmp = NullReference){
+                soReadFileBlock(ih,blockNum,buf);
+
+                for(uint32_t i = 2; i < DPB; i++){
+                    if(strcmp(buf[i].name, "\0") != 0){
+                        return false;
+                    }
+                }
+
+                blockCount++;
+            }
+
+            blockNum++;
+        }
+
+        return true;
+
     }
 };
 
