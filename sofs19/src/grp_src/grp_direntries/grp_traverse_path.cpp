71cd9a49a1a2dd54594a5cfa89c77d80e30f522f
danielvcorreia <danielvalacorreia@ua.pt>
2019-11-18 00:00:24 +0000

    TraversePath finished. Chenged bin calls. Error thrown in replenish when no more dz_free.

--- sofs19/src/grp_src/grp_direntries/grp_traverse_path.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_traverse_path.cpp
@@ -20,7 +20,35 @@ namespace sofs19
         soProbe(221, "%s(%s)\n", __FUNCTION__, path);
 
         /* change the following line by your code */
-        return binTraversePath(path);
+        
+        /* Checking if path is absolute */
+        if ( *path != '/' )
+            { throw SOException( EINVAL , __FUNCTION__ ); }
+
+        /* Recursive stop condition when path is "/" and return inode number 0 */
+        if ( strcmp( path, "/" ) == 0 )
+            { return 0; }
+
+        /* Replicate path */
+        char * baseName = basename( strdupa( path ) );
+        char * dirName = dirname( strdupa( path ) );
+        /* Get inode pointer */
+        uint32_t inode = soTraversePath( dirName );
+        uint32_t ih = soOpenInode( inode );
+        SOInode * inode_pointer = soGetInodePointer( ih );
+
+        /* Checking if inodes of path are dir ( S_IFDIR == 0040000 wich is */
+        /* the mask to check if this inode is a DIR ) */
+        if ( ( inode_pointer -> mode & S_IFDIR ) != S_IFDIR )
+            { throw SOException( ENOTDIR , __FUNCTION__ ); }
+        /* Checking if the process that calls this function has execute */
+        /* permission in all the components of the path */
+        if ( !soCheckInodeAccess( ih, X_OK ) )
+            { throw SOException( EACCES , __FUNCTION__ ); }
+        
+        uint32_t inode_number = soGetDirEntry( ih, baseName );
+        soCloseInode( ih );
+        return inode_number;
     }
 };
 
